2b420de91bec9a13853b756c30f396ce64af66cb
wijaya_mbkm2023 <wijaya.kus.war481605@mail.ugm.ac.id>
2025-06-19 09:40:59 +0700

    small

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import secrets
 import os
 from functools import wraps
 
-app = Flask(__name__, template_folder='templates', static_folder='../static/')
+app = Flask(__name__, template_folder='templates', static_folder='../static')
 app.secret_key = os.environ.get('SECRET_KEY', secrets.token_hex(32))
 
 # Timezone Indonesia
